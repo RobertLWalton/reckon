82e8a0a1d187432d20fdfc12b207a3edaac57dc2
Robert L Walton <walton@acm.org>
2017-11-06 07:24:31 -0500

    update

---
+++ src/reckon.cc
@@ -0,0 +1,21 @@
+// Reckon Interpreter/Compiler
+//
+// File:	reckon.cc
+// Author:	Bob Walton (walton@acm.org)
+// Date:	Mon Nov  6 07:06:01 EST 2017
+//
+// The authors have placed this program in the public
+// domain; they make no warranty and accept no liability
+// for this program.
+
+// Table of Contents
+//
+
+// Usage and Setup
+// ----- --- -----
+
+# include <ll_parser_standard.h>
+# define MUP min::unprotected
+# define PAR ll::parser
+
+int main () { return 0; }
