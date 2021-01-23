87065075c7baefdaa34e113a333589d2f42d2dbd
Robert L Walton <walton@acm.org>
2021-01-23 05:51:47 -0500

    update

--- src/reckon_parser.cc
+++ src/reckon_parser.cc
@@ -2,7 +2,7 @@
 //
 // File:	reckon_parser.cc
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Sat Dec 19 07:33:56 EST 2020
+// Date:	Sat Jan 23 05:49:36 EST 2021
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -53,22 +53,27 @@ void REC::init_parser ( PAR::parser parser )
     BRA::bracketed_pass bracketed_pass =
         (BRA::bracketed_pass) parser->pass_stack;
 
-    min::locatable_gen atom_name
-        ( min::new_str_gen ( "atom" ) );
     min::locatable_gen code_name
         ( min::new_str_gen ( "code" ) );
     min::locatable_gen math_name
         ( min::new_str_gen ( "math" ) );
+    min::locatable_gen text_name
+        ( min::new_str_gen ( "text" ) );
+    min::locatable_gen data_name
+        ( min::new_str_gen ( "data" ) );
 
-    TAB::flags atom =
-        1ull << TAB::find_name
-            ( parser->selector_name_table, atom_name );
     TAB::flags code =
         1ull << TAB::find_name
             ( parser->selector_name_table, code_name );
     TAB::flags math =
         1ull << TAB::find_name
             ( parser->selector_name_table, math_name );
+    TAB::flags text =
+        1ull << TAB::find_name
+            ( parser->selector_name_table, text_name );
+    TAB::flags data =
+        1ull << TAB::find_name
+            ( parser->selector_name_table, data_name );
 
     min::locatable_gen opening_quote
         ( min::new_str_gen ( "`" ) );
@@ -82,23 +87,35 @@ void REC::init_parser ( PAR::parser parser )
     BRA::push_brackets
         ( opening_quote,
 	  closing_quote,
-	  atom,
+	  code + math + text,
 	  block_level, PAR::top_level_position,
-	  TAB::new_flags ( 0, 0, 0 ),
+	  TAB::new_flags ( text, code + math + data, 0 ),
 	  min::NULL_STUB, min::NULL_STUB,
 	  bracketed_pass->bracket_table );
 
     BRA::push_brackets
         ( opening_square,
 	  closing_square,
-	  atom,
+	  text + code,
 	  block_level, PAR::top_level_position,
-	  TAB::new_flags ( code, atom, 0 ),
+	  TAB::new_flags ( math, text + code, 0 ),
 	  min::NULL_STUB, min::NULL_STUB,
 	  bracketed_pass->bracket_table );
+
 }
 
 # ifdef TBD
+
+
+    BRA::push_brackets
+        ( opening_quote,
+	  closing_quote,
+	  atom,
+	  block_level, PAR::top_level_position,
+	  TAB::new_flags ( 0, 0, 0 ),
+	  min::NULL_STUB, min::NULL_STUB,
+	  bracketed_pass->bracket_table );
+
     min::locatable_gen oper
         ( min::new_str_gen ( "operator" ) );
 
