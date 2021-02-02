fb854ecb275d6248025040b3db67ba23c15fd814
Robert L Walton <walton@acm.org>
2021-02-02 01:41:39 -0500

    update

--- src/reckon_parser.cc
+++ src/reckon_parser.cc
@@ -2,7 +2,7 @@
 //
 // File:	reckon_parser.cc
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Sat Jan 23 12:24:28 EST 2021
+// Date:	Tue Feb  2 01:26:45 EST 2021
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -97,8 +97,17 @@ void REC::init_parser ( PAR::parser parser )
         ( min::new_str_gen ( "]" ) );
     min::locatable_gen equal_number_sign
         ( min::new_str_gen ( "=#" ) );
+    min::locatable_gen if_name
+        ( min::new_str_gen ( "if" ) );
+    min::locatable_gen while_name
+        ( min::new_str_gen ( "while" ) );
+
     min::locatable_gen right_associative
         ( min::new_lab_gen ( "right", "associative" ) );
+
+    PAR::reformatter right_associative_reformatter =
+        PAR::find_reformatter
+	    ( right_associative, OP::reformatter_stack );
 
     BRA::push_brackets
         ( opening_quote,
@@ -124,10 +133,27 @@ void REC::init_parser ( PAR::parser parser )
 	  code + math,
 	  block_level, PAR::top_level_position,
 	  OP::INFIX,
-	  1000,
-	  PAR::find_reformatter
-	      ( right_associative,
-	        OP::reformatter_stack ),
+	  1000, right_associative_reformatter,
+	  min::NULL_STUB,
+	  oper_pass->oper_table );
+
+    OP::push_oper
+        ( if_name,
+	  min::MISSING(),
+	  code,
+	  block_level, PAR::top_level_position,
+	  OP::NOFIX,
+	  0, min::NULL_STUB,
+	  min::NULL_STUB,
+	  oper_pass->oper_table );
+
+    OP::push_oper
+        ( while_name,
+	  min::MISSING(),
+	  code,
+	  block_level, PAR::top_level_position,
+	  OP::NOFIX,
+	  0, min::NULL_STUB,
 	  min::NULL_STUB,
 	  oper_pass->oper_table );
 
