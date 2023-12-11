3fbb7c0cf8f233b9600139969df9ff358ead4d4b
Robert L Walton <walton@acm.org>
2023-12-11 12:36:33 +0000

    update

--- web/index.php
+++ web/index.php
@@ -6,6 +6,8 @@ $rec_method = $_SERVER['REQUEST_METHOD'];
 
 if ( $rec_method == 'POST' )
     require "$rec_home/server.php";
+else if ( isset ( $_GET['client'] ) )
+    require "$rec_home/client.html";
 else
     require "$rec_home/startup.html";
 ?>
