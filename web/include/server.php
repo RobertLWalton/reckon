071bf4ff892bb84d6be0f72f23f31835ca2b9089
Robert L Walton <walton@acm.org>
2023-12-28 20:04:49 -0500

    update

--- web/include/server.php
+++ web/include/server.php
@@ -2,25 +2,64 @@
 
 // File:   server.php
 // Author: Robert L Walton <walton@acm.org>
-// Date:   Wed Dec 27 00:48:32 EST 2023
+// Date:   Thu Dec 28 20:04:31 EST 2023
 // 
 // The authors have placed RECKON (its files and the
 // content of these files) in the public domain; they
 // make no warranty and accept no liability for RECKON.
 
+$time_format = "Y-m-d\Th:i:sT";
+
 if ( $method != 'POST' )
     exit ( "bad method: $method" );
 
-if ( ! isset ( $_POST['ID'] ) )
-    exit ( "no ID=..." );
-$ID = $_POST['ID'];
 session_name ( $session_name );
 session_start();
-if ( $ID != $_SESSION['ID'] )
-    exit ( "bad ID: $ID" );
+
+if ( ! isset ( $ipaddr_check ) )
+    $ipaddr_check = true;
+if (    $ipaddr_check 
+     && $_SESSION['IPADDR'] != $_SERVER['REMOTE_ADDR'] )
+    exit ( "IPADDR does not match" );
+
+if ( ! isset ( $_POST['ID'] ) )
+    exit ( "Request ID not set" );
+
+$ID = $_POST['ID'];
+
+if ( ! isset ( $_SESSION['LOG_TIME'] ) )
+    exit ( "Session LOG)TIME not set" );
+$log_time =
+    date ( $time_format,
+	   filemtime
+	       ( "$DDIR/log/$ID-start.log" ) );
+if ( $log_time != $_SESSION['LOG_TIME'] )
+    exit ( "bad log time: $log_time" );
 
 clearstatcache();
 umask ( 07 );
+
+$rundir = "$DDIR/runs/$ID";
+if ( is_dir ( $rundir ) )
+{
+    foreach ( scandir ( $rundir ) as $f )
+    {
+	if ( $f == "." ) continue;
+	if ( $f == ".." ) continue;
+	unlink ( "$rundir/$f" );
+    }
+    $list = scandir ( $rundir );
+    if ( count ( $list ) > 2 )
+	exit ( "could not unlink " .
+               implode ( ' ', $list ) );
+}
+else
+{
+    @mkdir ( "$DDIR/runs" );
+    @mkdir ( $rundir );
+    if ( ! is_dir ( $rundir ) )
+	exit ( "Could not make $rundir" );
+}
 
 if ( ! isset ( $_POST['op'] ) )
     exit ( "no op=..." );
