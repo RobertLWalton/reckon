e1568dfcc0a5a2833110599b53dd4cf3815a483c
Robert L Walton <walton@acm.org>
2023-12-29 07:40:12 -0500

    update

--- web/include/server.php
+++ web/include/server.php
@@ -2,7 +2,7 @@
 
 // File:   server.php
 // Author: Robert L Walton <walton@acm.org>
-// Date:   Thu Dec 28 20:04:31 EST 2023
+// Date:   Fri Dec 29 07:27:05 EST 2023
 // 
 // The authors have placed RECKON (its files and the
 // content of these files) in the public domain; they
@@ -61,6 +61,19 @@ else
 	exit ( "Could not make $rundir" );
 }
 
+// PHP symlink appears to fail inexplicably sometimes.
+//
+function symbolic_link ( $target, $link )
+{
+    return exec ( "ln -snf $target $link 2>&1" ) == '';
+}
+
+if ( exec ( "ln -snf $IDIR/reckon/test/reckon" .
+            " $rundir/reckon 2>&1" ) != '' )
+    exit ( "could not make symbolic link to reckon" );
+if ( ! is_file ( "$rundir/reckon" ) )
+    exit ( "reckon program does not exist" );
+
 if ( ! isset ( $_POST['op'] ) )
     exit ( "no op=..." );
 $op = $_POST['op'];
@@ -71,9 +84,45 @@ if ( ! isset ( $_POST['contents'] ) )
     exit ( "no contents=..." );
 $contents = $_POST['contents'];
 
+
+$pattern = '/\A([^\/.][^\/]*)\.rec\Z/';
+$r = preg_match ( $pattern, $filename, $matches );
+if ( $r !== 1 )
+    exit ( "$filename is not a legal .rec file name" );
+
+$base = $matches[1];
+
+if ( ! @file_put_contents ( "$rundir/$filename",
+                            $contents ) )
+    exit ( "Could not write $filename" );
+
+if ( ! @file_put_contents ( "$rundir/$base.status",
+                            "No Status Yet" . PHP_EOL) )
+    exit ( "Could not write $base.status" );
+
+
+$script = "";
+$script .= "trap 'exit 129' HUP" . PHP_EOL;
+    // If we do not do this, sending HUP
+    // returns exit code 0.
+$script .= "trap 'echo \$? >$base.exit' EXIT" . PHP_EOL;
+$script .= "echo $$ > $base.pid" . PHP_EOL;
+$script .= "set -e" . PHP_EOL;
+$script .= "cat < $filename" . PHP_EOL;
+if ( ! @file_put_contents ( "$rundir/$base.sh",
+                            $script ) )
+    exit ( "Could not write $base.sh" );
+
+exec ( "cd $rundir; " .
+       "setsid bash $base.sh " .
+       ">$base.out 2>$base.err & " );
+
+
 echo ( "ID = $ID" . PHP_EOL );
 echo ( "op = $op" . PHP_EOL );
 echo ( "filename = $filename" . PHP_EOL );
-echo ( $contents );
+$f = "$rundir/$base.out";
+$c = @file_get_contents ( $f );
+echo ( $c );
 
 ?>
